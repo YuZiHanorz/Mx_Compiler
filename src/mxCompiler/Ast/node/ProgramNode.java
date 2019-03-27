b7847130fd28f0fa2f077aa81f3ccd039844ea8c
zhyu18 <38875982+YuZiHanorz@users.noreply.github.com>
2019-03-27 15:23:04 +0800

    Update ProgramNode.java

--- src/mxCompiler/Ast/node/ProgramNode.java
+++ src/mxCompiler/Ast/node/ProgramNode.java
@@ -8,24 +8,30 @@ public class ProgramNode extends Node {
     public List<FuncDeclNode> globalFuncList;
     public List<ClassDeclNode> globalClassList;
     public List<VarDeclNode> globalVarList;
+    //fucking variables donot accept forward reference
+    public List<DeclNode> declList;
 
     public ProgramNode(){
         this.globalFuncList = new LinkedList<>();
         this.globalClassList = new LinkedList<>();
         this.globalVarList = new LinkedList<>();
+        this.declList = new LinkedList<>();
     }
 
     public void add(FuncDeclNode f){
         globalFuncList.add(f);
+        declList.add(f);
     }
 
     public void add(ClassDeclNode c){
         globalClassList.add(c);
+        declList.add(c);
     }
 
     // one line can declare many variables
     public void addAll(List<VarDeclNode> vL){
         globalVarList.addAll(vL);
+        declList.addAll(vL);
     }
 
     @Override

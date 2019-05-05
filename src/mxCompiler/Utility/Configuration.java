3be68494b275d9ab58ef1751d58f865c0a55345e
zhyu18 <38875982+YuZiHanorz@users.noreply.github.com>
2019-05-05 13:43:09 +0800

    eeeeeeeeeeeeeeeeee

--- src/mxCompiler/Utility/Configuration.java
+++ src/mxCompiler/Utility/Configuration.java
@@ -12,7 +12,7 @@ public class Configuration {
     public static int regSize = 8;
     public static boolean simple = true;
 
-    public static boolean useInlineOpt = false;
+    public static boolean useInlineOpt = true;
     public static int inlineMaxDepth = 3;
     public static int inlineOpCnt = 20;
 }

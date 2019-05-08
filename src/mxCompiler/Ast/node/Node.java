f9dffb0d0ed748d748912873b8f3901adf59f482
zhyu18 <38875982+YuZiHanorz@users.noreply.github.com>
2019-05-08 22:05:21 +0800

    to fit for my ugly Opt

--- src/mxCompiler/Ast/node/Node.java
+++ src/mxCompiler/Ast/node/Node.java
@@ -4,6 +4,7 @@ import mxCompiler.Ast.AstVisitor;
 import mxCompiler.Utility.Location;
 
 abstract public class Node{
+    public Node parent = null;
     public Location location = null;
 
     abstract public void accept(AstVisitor visitor);
